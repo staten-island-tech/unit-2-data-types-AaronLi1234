01646956c2135454588f5fd835ba97cebd2f4461
AaronLi1234 <158211987+AaronLi1234@users.noreply.github.com>
2024-02-06 13:58:18 -0500

    w

---
+++ lists.py
@@ -0,0 +1,19 @@
+"""x = 3
+y = float(3.55)
+print(x,y)"""
+"""x = True
+
+
+def check(x):
+    if(x > 6):
+         print("is logged in")
+    else:
+        print("please log in")
+check(x)
+x = 1241241241412412
+y = float(3.02)
+print(x,y)"""
+bill = int(input(float("what was your bill?")))
+tip = int(input("what percentage as a whole number out of 100 would you like to tip?"))
+total = int(f"{bill}+{tip}")
+print(f"your total is {total}")

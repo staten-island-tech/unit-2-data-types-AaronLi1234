829f93ffd1eb527b1f45075509b75592644d97ea
AaronLi1234 <158211987+AaronLi1234@users.noreply.github.com>
2024-02-07 13:58:15 -0500

    cool

---
+++ Word Counter.py
@@ -0,0 +1,7 @@
+sentence = input("write a sentence")
+def function(x):
+    input = x
+    y = input.split()
+    wordcount = len(y)
+    print(wordcount)
+function(sentence)
---
+++ tipcalculator.py
@@ -0,0 +1,22 @@
+"""x = 3
+y = float(3.55)
+print(x,y)"""
+"""x = True
+
+
+def check(x):
+    if(x > 6):
+         print("is logged in")
+    else:
+        print("please log in")
+check(x)
+x = 1241241241412412
+y = float(3.02)
+print(x,y)"""
+bill =float(input("what was your bill?"))
+tip = float(input("what percentage as a decimal out of 1, would you like to tip?"))
+"""def bill float(bill)"""
+total = float(bill) * float(tip) + float(bill)
+"""total = bill*tip/100
+total = int(f"{bill}+{tip}")"""
+print(f"your total is {total}")

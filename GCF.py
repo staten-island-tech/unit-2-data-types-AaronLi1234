f4491551f7081191144b8aeec3aaf7a47353e0bc
AaronLi1234 <158211987+AaronLi1234@users.noreply.github.com>
2024-02-09 13:40:07 -0500

    w

--- GCF.py
+++ GCF.py
@@ -1,6 +1,7 @@
 number1 = float(input("provide 2 numbers, and I will determine the GREATEST COMMON FACTOR between them"))
 number2 = float(input("can i get another one?"))
 x = 1
+y = 1
 factors1 = []
 while x <= number1:
     if number1 % x == 0:
@@ -8,11 +9,19 @@ while x <= number1:
         x+=1
     else:
         x+=1
-        x = 1
 factors2 = []
-while x <= number2:
-    if number2 % x == 0:
-        factors2.append(x)
-        x+=1
+while y <= number2:
+    if number2 % y == 0:
+        factors2.append(y)
+        y+=1
     else:
-        x+=1
+        y+=1
+commonfactors = []
+for i in factors1:
+    if i == i in factors2:
+        commonfactors.append(i)
+print(commonfactors)
+GCF = commonfactors[-1]
+print(GCF)
+
+

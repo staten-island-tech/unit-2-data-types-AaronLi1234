a82b38570e4fdd3e43de7ac0e4dc69f62705f21a
AaronLi1234 <158211987+AaronLi1234@users.noreply.github.com>
2024-02-14 13:57:39 -0500

    w

--- odd or even.py
+++ odd or even.py
@@ -1,7 +1,5 @@
 number = int(input("write a number"))
 realnumber = number % 2
-if realnumber == 0:
-    print("this number is even")
-else:
-    print("this number is odd")
+if realnumber == 0: print("this number is even")
+else: print("this number is odd")
 """print(%5)"""
